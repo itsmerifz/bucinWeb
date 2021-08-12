1ab827007fde0546609bf843fc6a92d69c4b0af7
Muhammad Arief <64636917+itsmerifz@users.noreply.github.com>
2021-08-12 13:55:33 +0800

    removing statement

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
 
-document.getElementById('tombol').addEventListener('click',function(){
-    document.querySelector('.popup').style.display = "flex";
-});
-
-
 function getNama(){
     var name = document.getElementById("mynama").value;
     localStorage.setItem("nama", name);
